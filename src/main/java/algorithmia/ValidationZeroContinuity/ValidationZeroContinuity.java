2ad1473dc5a25e78ab927b66689f996672828e9d
ons <adrian.harris@ons.gov.uk>
2019-03-19 14:21:33 +0000

    Algorithmia initial commit

---
+++ src/main/java/algorithmia/ValidationZeroContinuity/ValidationZeroContinuity.java
@@ -0,0 +1,13 @@
+package algorithmia.ValidationZeroContinuity;
+
+import com.algorithmia.*;
+import com.algorithmia.algo.*;
+import com.algorithmia.data.*;
+import com.google.gson.*;
+import java.util.*;
+
+public class ValidationZeroContinuity {
+    public String apply(String s) throws Exception {
+        return "Hello " + s;
+    }
+}
